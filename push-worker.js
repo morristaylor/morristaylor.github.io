809ed65c63ca64b37ef88fe724ef18abc3b61bcc
Taylor Morris <taylor.morris@gmail.com>
2018-08-14 14:01:10 -0700

    added snippet and push-worker

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1534280151.2
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://placeimg.com/150/150/animals/grayscale',
+  defaultTitle: 'Web Notify test',
+  defaultActionURL: 'http://taylormorris.net',
+  appKey: '6wY6UGf1ScqLxF8-vmBHkQ',
+  token: 'MTo2d1k2VUdmMVNjcUx4Rjgtdm1CSGtROlBuNTdvOUdSY0RHMmJDY0N1bnNkYVB0NDhHeHFGUkwtS3I5QktQOWJxYmM',
+  vapidPublicKey: 'BEpIXOBWX2T1SY6bxA3ZxHgy8-p36pt8qvfDZVEzUyeD5YK1rJtYqHBaGPQIe8K8jkpXN9aoi_HIitgJmF7Zp7E='
+})
