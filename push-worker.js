9541941a791a4d348598d26b65e59293daa1586e
Taylor Morris <taylor.morris@gmail.com>
2019-05-31 09:17:19 -0700

    updated push-worker
    now using Live channel

--- push-worker.js
+++ push-worker.js
@@ -1,10 +1,10 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1559313539.74
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1559319361.89
 importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://via.placeholder.com/200/92c952',
   defaultTitle: 'Taylor Morris dot Net',
-  defaultActionURL: 'https://taylormorris.net/',
-  appKey: 'r5Mu6ifuRMG2qT3YS5628A',
-  token: 'MTpyNU11NmlmdVJNRzJxVDNZUzU2MjhBOkk3VnVxUUpNdWFlTkVMRnl3SmJRQUZoOUQxT0lyQ0dfbWNZYjlvWDJVTjg',
-  vapidPublicKey: 'BCgf76Op1mJ0E16FKC8gmohIP2A0og8gtdOLtywecosXncpdE7ItiQbEAVGutD2-TLtUYOwfRut0OBkyeqGncIs='
+  defaultActionURL: 'https://taylormorris.net',
+  appKey: 'PmgrWJAXQy2G-MBcL_2dSw',
+  token: 'MTpQbWdyV0pBWFF5MkctTUJjTF8yZFN3OlI5SkRyS2RaV3Z3TnNEblZPWWUyanI5N0poc2ZQRG80MVRuRmpWLUg2eDA',
+  vapidPublicKey: 'BAE0ue8ERsfWaGHg9_78Q85uWQ2jK62H6zCbCFInSbJnGfrFtrA7QyA52chzyW6_PG9SbyBbko0mW1UOPKXnobA='
 })
