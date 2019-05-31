a2bd4f5bd9ed456eb208d440f3881e64e95b3b9f
Taylor Morris <taylor.morris@gmail.com>
2019-05-31 08:44:11 -0700

    added push-worker.js
    for Airship notify

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1559313539.74
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://via.placeholder.com/200/92c952',
+  defaultTitle: 'Taylor Morris dot Net',
+  defaultActionURL: 'http://taylormorris.net/',
+  appKey: 'r5Mu6ifuRMG2qT3YS5628A',
+  token: 'MTpyNU11NmlmdVJNRzJxVDNZUzU2MjhBOkk3VnVxUUpNdWFlTkVMRnl3SmJRQUZoOUQxT0lyQ0dfbWNZYjlvWDJVTjg',
+  vapidPublicKey: 'BCgf76Op1mJ0E16FKC8gmohIP2A0og8gtdOLtywecosXncpdE7ItiQbEAVGutD2-TLtUYOwfRut0OBkyeqGncIs='
+})
