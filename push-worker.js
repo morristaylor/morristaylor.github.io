77596466fc8d8a7ad9844074d803e9c60c9edb66
Taylor Morris <taylor.morris@gmail.com>
2019-05-31 09:02:07 -0700

    updated push-worker.js
    http to https

--- push-worker.js
+++ push-worker.js
@@ -3,7 +3,7 @@ importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://via.placeholder.com/200/92c952',
   defaultTitle: 'Taylor Morris dot Net',
-  defaultActionURL: 'http://taylormorris.net/',
+  defaultActionURL: 'https://taylormorris.net/',
   appKey: 'r5Mu6ifuRMG2qT3YS5628A',
   token: 'MTpyNU11NmlmdVJNRzJxVDNZUzU2MjhBOkk3VnVxUUpNdWFlTkVMRnl3SmJRQUZoOUQxT0lyQ0dfbWNZYjlvWDJVTjg',
   vapidPublicKey: 'BCgf76Op1mJ0E16FKC8gmohIP2A0og8gtdOLtywecosXncpdE7ItiQbEAVGutD2-TLtUYOwfRut0OBkyeqGncIs='
