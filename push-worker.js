dc7e892e792a35518a71be151d6e43bb178c1686
Taylor Morris <taylor.morris@gmail.com>
2019-05-31 08:31:10 -0700

    set up Airship web notify

--- push-worker.js
+++ push-worker.js
@@ -1,10 +1,10 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1534275833.6
-importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1559313539.74
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
-  defaultIcon: 'https://placeimg.com/150/150/animals/grayscale',
-  defaultTitle: 'Web Notify test',
-  defaultActionURL: 'http://taylormorris.net',
-  appKey: '6wY6UGf1ScqLxF8-vmBHkQ',
-  token: 'MTo2d1k2VUdmMVNjcUx4Rjgtdm1CSGtROlBuNTdvOUdSY0RHMmJDY0N1bnNkYVB0NDhHeHFGUkwtS3I5QktQOWJxYmM',
-  vapidPublicKey: 'BEpIXOBWX2T1SY6bxA3ZxHgy8-p36pt8qvfDZVEzUyeD5YK1rJtYqHBaGPQIe8K8jkpXN9aoi_HIitgJmF7Zp7E='
+  defaultIcon: 'https://via.placeholder.com/200/92c952',
+  defaultTitle: 'Taylor Morris dot Net',
+  defaultActionURL: 'http://taylormorris.net/',
+  appKey: 'r5Mu6ifuRMG2qT3YS5628A',
+  token: 'MTpyNU11NmlmdVJNRzJxVDNZUzU2MjhBOkk3VnVxUUpNdWFlTkVMRnl3SmJRQUZoOUQxT0lyQ0dfbWNZYjlvWDJVTjg',
+  vapidPublicKey: 'BCgf76Op1mJ0E16FKC8gmohIP2A0og8gtdOLtywecosXncpdE7ItiQbEAVGutD2-TLtUYOwfRut0OBkyeqGncIs='
 })
